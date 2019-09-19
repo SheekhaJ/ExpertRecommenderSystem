b700ab0832baed5ba9c8224e1c69e4c9a042c011
SheekhaJ <jariwals@oregonstate.edu>
2019-09-19 14:48:10 -0700

    Initial code for front-end

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Header, Footer} from './components/Layouts/index'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      {/* <Footer/> */}
+    </div>
+  );
+}
+
+export default App;
